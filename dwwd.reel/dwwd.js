95a3e314fa57bdfda6ebe66a30dcc8f6683b28f0
nunoboleto <noreply>
2015-08-20 23:12:32 +0000

    Add component dwwd

---
+++ dwwd.reel/dwwd.js
@@ -0,0 +1,16 @@
+/**
+ * @module //dwwd.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Dwwd
+ * @extends Component
+ */
+exports.Dwwd = Component.specialize(/** @lends Dwwd# */ {
+    constructor: {
+        value: function Dwwd() {
+            this.super();
+        }
+    }
+});
